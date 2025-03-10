ac3195f51ce6dadb0708f62cf298c9c469a90163
online-marathon <61456363+online-marathon@users.noreply.github.com>
2025-03-10 14:35:02 +0200

    Update TaskServiceTest.java

--- src/test/java/com/softserve/itacademy/TaskServiceTest.java
+++ src/test/java/com/softserve/itacademy/TaskServiceTest.java
@@ -1,30 +1,34 @@
 package com.softserve.itacademy;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.softserve.itacademy.model.User;
 import com.softserve.itacademy.service.UserService;
 
-@RunWith(JUnitPlatform.class)
+@ExtendWith(SpringExtension.class) 
 public class TaskServiceTest {
-    private static UserService userService;
 
-    @BeforeAll
-    public static void setupBeforeClass() throws Exception {
-        AnnotationConfigApplicationContext annotationConfigContext = new AnnotationConfigApplicationContext(Config.class);
+    private UserService userService;
+    private AnnotationConfigApplicationContext annotationConfigContext;
+
+    @BeforeEach
+    public void setup() {
+        annotationConfigContext = new AnnotationConfigApplicationContext(Config.class);
         userService = annotationConfigContext.getBean(UserService.class);
+    }
+
+    @AfterEach
+    public void tearDown() {
         annotationConfigContext.close();
     }
 
     @Test
     public void checkAddTask() {
-        // TODO, code
+        Assertions.assertNotNull(userService);
     }
 
-    // TODO, other tests
+    // TODO: other tests
 }
